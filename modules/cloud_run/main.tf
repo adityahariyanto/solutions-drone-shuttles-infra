8bd9bb74caf7a60d371f61352038d4cca3d3a792
Aditya Hariyanto <aditya.hariyanto@nordcloud.com>
2022-04-07 18:23:40 +0200

    Hotfix

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -72,9 +72,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  for_each = toset(local.locations)
+  for_each = toset(var.locations)
 
-  service     = module.cloud_run[each.key].name
+  service     = module.cloud_run[each.key].service_name
   location    = module.cloud_run[each.key].location
   policy_data = data.google_iam_policy.noauth.policy_data
 }
