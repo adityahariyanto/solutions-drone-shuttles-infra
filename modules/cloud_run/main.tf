29dde91d01b9d726234c2a23fce56267ceee17dd
Aditya Hariyanto <aditya.hariyanto@nordcloud.com>
2022-04-07 17:49:20 +0200

    Added NEG

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -12,3 +12,19 @@ module "cloud_run" {
       "port": 2368
   }
 }
+
+resource "google_compute_global_adress" "ip" {
+  name = "${var.env}-service-ip"
+}
+
+resource "google_compute_region_network_endpoint_group" "neg" {
+  for_each = toset(var.locations)
+
+  name = "neg-${each.key}"
+  network_endpoint_type = "SERVERLESS"
+  region = each.key
+
+  cloud_run {
+    service = module.cloud_run[each.key].service_name
+  }
+}
