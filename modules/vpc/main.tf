30120cb92f02859c32414851e4b998a9342d23e5
Aditya Hariyanto <aditya.hariyanto@nordcloud.com>
2022-04-08 13:45:12 +0200

    Hotfix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,27 +1,30 @@
-module "subnet_addrs" {
-  source   = "hashicorp/subnets/cidr"
-  for_each = toset(var.locations)
-
-  base_cidr_block = "10.0.0.0/16"
-
-  networks = [
-    {
-      name     = "${var.env}-subnet-${each.key}",
+locals {
+  subnets = [
+    for loc in var.locations : {
+      name     = "${var.env}-subnet-${loc}"
       new_bits = 8
     }
   ]
 }
 
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = "10.0.0.0/16"
+
+  networks = local.subnets
+}
+
 module "vpc" {
-  source   = "terraform-google-modules/network/google"
-#   for_each = module.subnet_addrs.network_cidr_blocks
+  source = "terraform-google-modules/network/google"
+  #   for_each = module.subnet_addrs.network_cidr_blocks
 
   project_id   = var.project
   network_name = "${var.env}-network"
 
 
   subnets = [
-      for n in module.subnet_addrs.networks : {
+    for n in module.subnet_addrs.networks : {
       subnet_name   = n.name
       subnet_ip     = n.cidr_block
       subnet_region = element(split("-", n.name), 2)
@@ -30,8 +33,8 @@ module "vpc" {
     if n.cidr_block != null
   ]
 
-  secondary_ranges = {
-    "${each.key}" = []
-  }
+  #   secondary_ranges = {
+  #     "${each.key}" = []
+  #   }
 
 }
