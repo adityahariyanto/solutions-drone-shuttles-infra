30258d27f289d14f4b56330770beccecb9bd41df
Aditya Hariyanto <aditya.hariyanto@nordcloud.com>
2022-04-08 11:26:45 +0200

    Hotfix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,38 +1,35 @@
 module "subnet_addrs" {
-  source = "hashicorp/subnets/cidr"
-
-  base_cidr_block = "10.0.0.0/22"
-
+  source   = "hashicorp/subnets/cidr"
   for_each = toset(var.locations)
+
+  base_cidr_block = "10.0.0.0/16"
+
   networks = [
     {
       name     = "${var.env}-subnet-${each.key}",
-      new_bits = 2
-    },
-
+      new_bits = 8
+    }
   ]
 }
 
 module "vpc" {
-  source = "terraform-google-modules/network/google"
+  source   = "terraform-google-modules/network/google"
+  for_each = module.subnet_addrs.network_cidr_blocks
 
   project_id   = var.project
   network_name = "${var.env}-network"
 
-  for_each = module.subnet_addrs.networks
+
   subnets = [
     {
       subnet_name   = each.key
       subnet_ip     = each.value
       subnet_region = element(split("-", each.key), 2)
-    },
+    }
   ]
 
   secondary_ranges = {
     "${each.key}" = []
   }
 
-  depends_on = [
-    module.subnet_addrs
-  ]
 }
