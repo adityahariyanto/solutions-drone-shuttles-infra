1b778d4b74f67bb6b53e9f8ef371c36e9858e338
Aditya Hariyanto <aditya.hariyanto@nordcloud.com>
2022-04-08 14:30:10 +0200

    Hotfix

--- modules/cloud_sql/main.tf
+++ modules/cloud_sql/main.tf
@@ -1,31 +1,33 @@
 resource "google_sql_database_instance" "mysql_db" {
-  name = "my-db"
+  name             = "my-db"
   database_version = "MYSQL_5_7"
-  region = "europe-west6"
+  region           = "europe-west6"
 
   settings {
     tier = "db-f1-micro"
 
     database_flags {
-        name = "cloudsql.iam_authentication"
-        value = "on"
+      name  = "cloudsql.iam_authentication"
+      value = "on"
     }
   }
 }
 
 resource "google_sql_user" "iam_user" {
-    name = "aditya.hariyanto@nordcloud.com"
-    instance = google_sql_database_instance.mysql_db.name
-    type = "CLOUD_IAM_USER"
-  
+  name     = "aditya.hariyanto@nordcloud.com"
+  instance = google_sql_database_instance.mysql_db.name
+  type     = "CLOUD_IAM_USER"
+
 }
 
 resource "google_project_iam_member" "iam_user_cloudsql_instance_user" {
-  role = "roles/cloudsql.instanceUser"
-  member = format("user:%s", google_sql_user.iam_user.name)
+  role    = "roles/cloudsql.instanceUser"
+  member  = format("user:%s", google_sql_user.iam_user.name)
+  project = "drone-shuttles-dev"
 }
 
 resource "google_project_iam_member" "iam_user_cloudsql_client" {
-  role = "roles/cloudsql.client"
-  member = format("user:%s", google_sql_user.iam_user.name)
+  role    = "roles/cloudsql.client"
+  member  = format("user:%s", google_sql_user.iam_user.name)
+  project = "drone-shuttles-dev"
 }
